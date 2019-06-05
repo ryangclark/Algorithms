208afc9dbeedee7ba99fd72b1c1175006624a65a
Clark <rclark010@gmail.com>
2019-06-05 11:54:55 -0600

    Completes challenge

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,23 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  if not type(prices) == type([]):
+    return 'Input must be type list'
+  
+  if len(prices) < 2:
+    return 'Input must be list with minimum length of 2'
 
+  max = prices[1] - prices[0]
+
+  for index, item in enumerate(prices):
+    for i in prices[index + 1:]:
+      if  i - item > max:
+        max = i - item
+        print('item', item, 'i', i)
+       
+  return max
+
+print(find_max_profit([100]))
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
